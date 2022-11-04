4bb5da4c8f69b1cca3a1899bf1d7d57fab85cad0
Carl <carlelli5@hotmail.co.uk>
2022-11-04 13:06:41 +0000

    Added function to type letters slowly in terminal

--- run.py
+++ run.py
@@ -2,17 +2,26 @@
 Battleships game that runs in a custom terminal
 """
 
+# Libraries imported into the game
 import os
 import time
 from art import text2art
+
+
+def type_slow(word):
+    """
+    Types letters slowley in terminal
+    """
+    for letter in word:
+        time.sleep(0.1)
+        print(letter, end="", flush=True)
 
 
 def game_load():
     """
     Shows in terminal when first accessing battleships game.
     """
-    print("Game Loading....\n")
-    time.sleep(2)
+    print(type_slow("Game Loading....\n"))
     os.system("clear")
     art = text2art("Battle Ships")
     print(art)
