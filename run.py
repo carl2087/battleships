3e9190a003fc8b14869ac41163c09b53f805e49e
Carl <carlelli5@hotmail.co.uk>
2022-11-04 13:50:33 +0000

    Added function to print game rules to terminal

--- run.py
+++ run.py
@@ -34,6 +34,24 @@ def game_load():
     os.system("clear")
     art = text2art("Battle Ships")
     print(type_fast(art))
+    os.system("clear")
+
+
+def game_rules():
+    """
+    Prints the games rules to terminal
+    """
+    time.sleep(1.5)
+    type_fast("The aim of the game is to sink all of your ")
+    type_fast("opponents ships.\n")
+    type_fast("You and the computer will each have their own ")
+    type_fast("battlefield grid\n")
+    type_fast("Place your ships strategically.\n")
+    type_fast("You cannot see your opponents ships ")
+    type_fast("and they cannot see yours.\n")
+    type_fast("Whoever sinks all of the opponents ")
+    type_fast("ships first wins the game.\n")
 
 
 game_load()
+game_rules()
