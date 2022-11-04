87b48e37722e78eca13388dd38b00b4db056174b
Carl <carlelli5@hotmail.co.uk>
2022-11-04 11:19:24 +0000

    Added function game_load for when battleships first loads in terminal

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-from art import *
+"""
+Battleships game that runs in a custom terminal
+"""
 
-tprint("test")
+import os
+import time
+from art import text2art
+
+
+def game_load():
+    """
+    Shows in terminal when first accessing battleships game.
+    """
+    print("Game Loading....\n")
+    time.sleep(2)
+    os.system("clear")
+    art = text2art("Battle Ships")
+    print(art)
+
+
+game_load()
